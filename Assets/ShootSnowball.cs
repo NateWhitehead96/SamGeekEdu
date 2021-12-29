505362ac62f75db6704f498698642d7fb771cb71
NateWhitehead96 <50887818+NateWhitehead96@users.noreply.github.com>
2021-12-28 23:38:51 -0500

    Initial commit

---
+++ Assets/ShootSnowball.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootSnowball : MonoBehaviour
+{
+    public GameObject snowball; // this will be a refrence to our snowball prefab
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
